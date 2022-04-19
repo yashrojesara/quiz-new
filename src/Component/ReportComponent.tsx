9dd3f74b4db3f94e14d2e39a25acea09017ba725
Yash Rojesara <yashsoni9861@gmail.com>
2022-04-19 09:39:01 +0530

    Added chart

--- src/Component/ReportComponent.tsx
+++ src/Component/ReportComponent.tsx
@@ -1,9 +1,32 @@
 import { useQuiz } from "react-quiz-wizard";
+import { Chart } from "react-google-charts";
 
 const ReportComponent: React.FC = () => {
   const { generateReport } = useQuiz();
   console.log(generateReport());
-  return <></>;
+
+  const chartData = generateReport().chartData;
+  const correctAnswer = chartData[0].value;
+  const InCorrectAnswer = chartData[1].value;
+  const UnAnswered = chartData[2].value;
+
+  return (
+    <>
+      <Chart
+        chartType="PieChart"
+        data={[
+          ["Correct Answer", "InCorrect Answer"],
+          ["Correct", correctAnswer],
+          ["InCorrect", InCorrectAnswer],
+          ["UnAnswered", UnAnswered],
+        ]}
+        options={{ title: "Result" }}
+        width="100%"
+        height="400px"
+        legendToggle
+      />
+    </>
+  );
 };
 
 export default ReportComponent;
